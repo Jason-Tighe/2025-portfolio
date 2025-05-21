38d7ea93aff0859b4860b8fefb28415ef5d11393
Jason Tighe <tighe59@gmail.com>
2025-05-21 16:40:16 -0400

    finally got tailwind to cooperate

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>
 )
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+    return (
+        <div className="bg-red-400">
+            <h1>Welcome to the Home Page</h1>
+            <p>This is the home page of our application.</p>
+        </div>
+    );
+}
